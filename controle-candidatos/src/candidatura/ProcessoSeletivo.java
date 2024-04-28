bb303b8bd4def1ee82f3340a68902ae54636daf3
Lucas Phelipe Castanho Ribeiro <135936579+LucasCastanh0@users.noreply.github.com>
2024-04-28 16:34:03 -0300

    METHOD SELECAOCANDIDATOS

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -2,7 +2,18 @@ package candidatura;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        System.out.println("Processo seletivo");
+       
+    }
+
+    static void selecaoCandidatos(){
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO","AUGUSTO", "MONICA", "FRABRICIO", "MIRELA", "DANIELA", "JORGE" };
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5) {
+            String candidato = candidatos [candidatosAtual];
+        }
     }
 
     static void analisarCandidato(Double salarioPretendido){
