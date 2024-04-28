680fb3ebb32c27c2eaea458295f0f3ebd7fbb071
Lucas Phelipe Castanho Ribeiro <135936579+LucasCastanh0@users.noreply.github.com>
2024-04-28 16:54:12 -0300

    DECISION STRUCTURE IN SELECAOCANDIDATOS

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -12,13 +12,17 @@ public class ProcessoSeletivo {
         int candidatosSelecionados = 0;
         int candidatosAtual = 0;
         double salarioBase = 2000.0;
-        while (candidatosSelecionados < 5) {
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
             String candidato = candidatos [candidatosAtual];
             double salarioPretendido = valorPretendido();
 
-            System.out.println("O candidato" + candidato + "Solicitou este valor de salário" + salarioPretendido);
+            System.out.println("O candidato" + candidato + "solicitou este valor de salário" + salarioPretendido);
+            if (salarioBase <= salarioPretendido) {
+                System.out.println("O candidato" + candidato + "foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
         }
-
     }
     static double valorPretendido() {
         return ThreadLocalRandom.current().nextDouble(1800, 2200);
